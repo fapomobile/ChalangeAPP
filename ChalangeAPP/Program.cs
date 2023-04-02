122e56fddb642179249fbd28b3a5b82cf4cfd0bc
fapomobile <a.terwinski@o2.pl>
2023-04-02 12:43:30 +0200

    zmiany  if i else

--- ChalangeAPP/Program.cs
+++ ChalangeAPP/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Adam! Jestem gotowy do pracy.");
+﻿// See https://aka.ms/new-console-template for more informatio
+int number1 = 5;
+int number2 = 10;
+
+
+var name = "adam";
+var age = 50;
+
+if (name == "adam" && age < 60)
+{
+    Console.WriteLine("jESTEM Adamem lub mam mniej niż 60 at");
+}
+else if (age > 40)
+{
+    Console.WriteLine("jESTEM W wieku 40 lat");
+}
+else if (age > 30) ;
+{
+    Console.WriteLine("jESTEM W wieku 30 lat");
+}
+// OPERATORY RELACYJNE 
+// ==   równe
+// <=  lub
+// >=
+// !=  są różne
+
+// operator logiczny 
+// && i 
+// || lub 
+// ! not - negacja
